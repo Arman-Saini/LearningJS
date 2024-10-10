a3112d941afd2bc000963374c5670afce9b622ec
Arman-Saini <144936435+Arman-Saini@users.noreply.github.com>
2024-10-10 19:09:14 +0000

    conversion of data types

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,35 @@
+let score = true
+
+
+console.log(typeof(score))
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let loggedIn = 1
+
+let booleanLoggedIn = Boolean(loggedIn)
+console.log(loggedIn)
+console.log(booleanLoggedIn)
+console.log(typeof booleanLoggedIn)
+
+
+let someNumber = 69
+
+let numberInString = String(someNumber)
+
+console.log(numberInString);
+console.log(typeof numberInString)
+
+
+//1 => true; 0 => false
+// "" => false
+// "someString" => true
