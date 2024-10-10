897d9067b396bd1db97ae7d0eac559130310c95f
Arman-Saini <144936435+Arman-Saini@users.noreply.github.com>
2024-10-10 18:45:16 +0000

    datatypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,21 @@
+const accountId = 5;
+// accountId = 1;
+
+let Email = "jasnehkaurgillehri@gmail.com"
+
+var accountPassword = "JasnehIsACutiePatootie!"
+
+accountCity = "New Delhi"
+
+let accountState;
+
+console.table([accountId, Email, accountPassword, accountCity, accountState])
+
+let name = "arman"
+let isBitchless = true
+let willStayALoserHisEntireLife = false
+console.log(typeof("amongus"))
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
